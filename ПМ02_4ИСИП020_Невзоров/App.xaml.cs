4348ae95f8f289df0607da2f2633d11056276c23
208537 <208537@KB37-1-120-C01>
2024-04-18 16:13:36 +0300

    Добавьте файлы проекта.

---
+++ ПМ02_4ИСИП020_Невзоров/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ПМ02_4ИСИП020_Невзоров
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
